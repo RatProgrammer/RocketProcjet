f229ddb117a347b8b8ea91af1e413d75c1c034c6
Olo ;) <15olo1995@gmail.com>
2016-03-30 22:59:15 +0200

    Partly implemented ALE , to do: refactoring ALE, ends dependences of score

--- praktyki_2016/src/WPFP.Core/AfterLaunchScoringClass.cs
+++ praktyki_2016/src/WPFP.Core/AfterLaunchScoringClass.cs
@@ -1,9 +1,8 @@
-﻿using WPFP.CommunicationLayer.DTO;
-using WPFP.CommunicationLayer.Enums;
+﻿using WPFP.CommunicationLayer.Enums;
 using WPFP.CommunicationLayer.Interfaces;
-using WPFP.Core.ParsingStuff;
+using WPFP.Psyche;
 using WPFP.Psyche.StateMachine;
-using WPFP.Psyche.StateMachine.DomainListener.Primary.Research;
+using WPFP.SimConfiguration.Dependency;
 
 namespace WPFP.Core
 {
@@ -13,20 +12,58 @@ namespace WPFP.Core
 
         public AfterLaunchScoringMechanism(ScoringMechanism scoringMechanism)
         {
-           
-            _scoringMechanism = scoringMechanism;
-           
+           _scoringMechanism = scoringMechanism;
         }
 
         public void Launch()
         {
             GoalType goalType = _scoringMechanism.GoalType();
-            bool wasSomeKingOfEventPublished = _scoringMechanism.WasEventPublished("someKindOfEvent");
-            _scoringMechanism.Increase(15);
+            SmartCityAchivment(goalType);
+            HealthCareAchivment(goalType);
+            ResearchedCocpitAchivment();
+            RocetAssembledAchivment();
 
-         
         }
-       
+
+        private void RocetAssembledAchivment()
+        {
+            if (_scoringMechanism.WasEventPublished(EventType.RocketAssembled.ToString()))
+            {
+                _scoringMechanism.Increase(1000);
+            }
+        }
+
+        private void ResearchedCocpitAchivment()
+        {
+            if (_scoringMechanism.WasEventPublished(EventType.ResearchedCockpit.ToString()))
+            {
+                _scoringMechanism.Increase(1000);
+            }
+        }
+
+        private void SmartCityAchivment(GoalType goalType)
+        {
+            if (goalType == GoalType.SmartCity)
+            {
+                if (_scoringMechanism.WasEventPublished(EventType.UniversalBatteryResearched.ToString()) ||
+                    _scoringMechanism.WasEventPublished(EventType.MoreEfficientTransportationTakesWorldByStorm.ToString()))
+                {
+                    _scoringMechanism.Increase(1000);
+                }
+            }
+        }
+
+        private void HealthCareAchivment(GoalType goalType)
+        {
+            if (goalType == GoalType.Healthcare)
+            {
+                if (_scoringMechanism.WasEventPublished(EventType.CleanerFuelResearched.ToString()) ||
+                    _scoringMechanism.WasEventPublished(EventType.SafeStimulantsSold.ToString()))
+                {
+                    _scoringMechanism.Increase(1000);
+                }
+            }
+        }
     }
     
 }
