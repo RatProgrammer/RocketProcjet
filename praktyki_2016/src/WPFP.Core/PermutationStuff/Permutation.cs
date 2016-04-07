9f1ff1bbf1691199fb189934cb7e628419b19916
Olo ;) <15olo1995@gmail.com>
2016-04-07 19:06:15 +0200

    17800 pkt

---
+++ praktyki_2016/src/WPFP.Core/PermutationStuff/Permutation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFP.CommunicationLayer.DTO;
+
+namespace WPFP.Core.PermutationStuff
+{
+    class Permutation
+    {
+        public IEnumerable<IEnumerable<string>> DoPermutation(ReportInfo reportInfo)
+        {
+           return null;
+        }
+    }
+}
